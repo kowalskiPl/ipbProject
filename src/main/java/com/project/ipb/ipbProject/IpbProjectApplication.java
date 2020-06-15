e0da76f524d72335166558ee924cf602f0c64cef
Patryk Kobus <48224476+kowalskiPl@users.noreply.github.com>
2020-06-15 14:42:05 +0200

    Initial commit

---
+++ src/main/java/com/project/ipb/ipbProject/IpbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.ipb.ipbProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpbProjectApplication.class, args);
+	}
+
+}
